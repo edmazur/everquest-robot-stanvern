edmazur/everquest-robot-stanvern

commit e9a59d6a2157282b02c3c470c16df43f3f67d162

Preserve input's upper/lower-casing in GratsParser error messages.

src/main/java/com/edmazur/eqrs/game/listener/GratsParser.java
@@ -82,22 +82,22 @@ public class GratsParser {
     if (!matcher.matches()) {
       return ValueOrError.error("Error reading guild chat");
     }
-    String gratsMessage = matcher.group(1).toLowerCase();
+    String gratsMessage = matcher.group(1);
 
     // Remove the trigger.
     for (String trigger : GratsDetector.TRIGGERS) {
-      gratsMessage = gratsMessage.replace(trigger, "");
+      gratsMessage = gratsMessage.replaceAll("(?i)" + trigger, "");
     }
 
     // Remove the item name.
-    gratsMessage = gratsMessage.replace(item.getName().toLowerCase(), "");
+    gratsMessage = gratsMessage.replaceAll("(?i)" + item.getName(), "");
 
     // Group what's remaining into categories.
     List<String> alphaOnlyParts = new ArrayList<String>();
     List<Integer> numericOnlyParts = new ArrayList<Integer>();
     List<String> mixedParts = new ArrayList<String>();
     for (String part : gratsMessage.trim().split("\\s+")) {
-      if (part.matches("[a-z]+")) {
+      if (part.matches("[a-zA-Z]+")) {
         alphaOnlyParts.add(part);
       } else if (part.matches("[0-9]+")) {
         numericOnlyParts.add(Integer.parseInt(part));

src/test/java/com/edmazur/eqrs/game/listener/GratsParserTest.java
@@ -81,7 +81,7 @@ class GratsParserTest {
         + "'!Grats  Nature's Melody 650 Closed Bigdumper'").get();
     gratsParser.parse(eqLogEvent);
     assertEquals(
-        "Multiple name candidates found (closed, bigdumper)",
+        "Multiple name candidates found (Closed, Bigdumper)",
         lootCommandOrError.getError());
   }
 

commit c7773ed26a40661d9b323da472393967f2db6761

Ensure that names in $loot commands have only the first letter capitalized.
https://github.com/edmazur/everquest-robot-stanvern/commit/e9a59d6a2157282b02c3c470c16df43f3f67d162 inadvertently removed this behavior.

src/main/java/com/edmazur/eqrs/game/listener/GratsParser.java
@@ -144,7 +144,7 @@ public class GratsParser {
       return ValueOrError.error(
           "Multiple name candidates found: `" + Joiner.on("`, `").join(alphaOnlyParts) + "`");
     }
-    String playerName = StringUtils.capitalize(alphaOnlyParts.get(0));
+    String playerName = StringUtils.capitalize(alphaOnlyParts.get(0).toLowerCase());
 
     // If you've gotten this far, there is a single name and number, so you can assume it's a player
     // name and DKP amount.

src/test/java/com/edmazur/eqrs/game/listener/GratsParserTest.java
@@ -142,6 +142,15 @@ class GratsParserTest {
   }
 
   @Test
+  void lootStringUppercaseName() {
+    EqLogEvent eqLogEvent = EqLogEvent.parseFromLine(
+        "[Tue May 23 17:31:38 2023] Faldimir tells the guild, "
+        + "'!grats Spear of Fate 400 HAMFORK'").get();
+    gratsParser.parse(eqLogEvent);
+    assertEquals("$loot Spear of Fate Hamfork 400", lootCommandOrError.getValue());
+  }
+
+  @Test
   void lootStringBacktickItem() {
     EqLogEvent eqLogEvent = EqLogEvent.parseFromLine(
         "[Sun Jun 04 23:32:34 2023] Mccreary tells the guild, "