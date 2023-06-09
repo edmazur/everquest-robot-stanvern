edmazur/everquest-robot-stanvern

commit 20ccd277f523939c51001358d2642a8a83d6b34a

Upgrade Javacord from 3.4.0 to 3.8.0.

src/main/java/com/edmazur/eqrs/discord/Discord.java
@@ -18,6 +18,7 @@ import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.ChannelCategory;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.intent.Intent;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.server.Server;
@@ -32,6 +33,7 @@ public class Discord {
   public Discord(Config config) {
     discordApi = new DiscordApiBuilder()
         .setToken(config.getString(Config.Property.DISCORD_PRIVATE_KEY))
+        .addIntents(Intent.MESSAGE_CONTENT)
         .login()
         .join();
 

src/main/java/com/edmazur/eqrs/discord/listener/AuditListener.java
@@ -14,7 +14,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 import org.javacord.api.entity.channel.ServerChannel;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.event.message.MessageCreateEvent;
@@ -82,18 +81,12 @@ public class AuditListener implements MessageCreateListener, MessageEditListener
 
   @Override
   public void onMessageEdit(MessageEditEvent event) {
-    try {
-      onMessage(
-          event.getChannel(),
-          event.requestMessage().get().getLastEditTimestamp().get(),
-          event.getMessageAuthor().isPresent()
-              ? event.getMessageAuthor().get().getDisplayName() : "",
-          event.getNewContent(),
-          MessageType.EDIT);
-    } catch (InterruptedException | ExecutionException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    onMessage(
+        event.getChannel(),
+        event.getMessage().getLastEditTimestamp().get(),
+        event.getMessageAuthor().getDisplayName(),
+        event.getMessageContent(),
+        MessageType.EDIT);
   }
 
   private void onMessage(

build.gradle
@@ -24,7 +24,7 @@ dependencies {
   implementation 'mysql:mysql-connector-java:8.0.25'
   implementation 'org.ahocorasick:ahocorasick:0.6.3'
   implementation 'org.apache.commons:commons-text:1.9'
-  implementation 'org.javacord:javacord:3.4.0'
+  implementation 'org.javacord:javacord:3.8.0'
   implementation 'org.seleniumhq.selenium:selenium-java:4.9.0'
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("org.mockito:mockito-core:3.+")

src/main/java/com/edmazur/eqrs/discord/listener/CharInfoScreenshotListener.java
@@ -62,7 +62,7 @@ public class CharInfoScreenshotListener implements MessageCreateListener {
         List<MessageAttachment> messageAttachments = message.getAttachments();
         for (MessageAttachment messageAttachment : messageAttachments) {
           if (messageAttachment.isImage()) {
-            ImageIO.write(messageAttachment.downloadAsImage().join(), "png", image);
+            ImageIO.write(messageAttachment.asImage().join(), "png", image);
             break;
           }
         }

commit 1c0c3d52161cece23a9547a7f2e7616d20328ef6

Try to fix the exception in AuditListener.onMessageEdit() that sometimes happens when messages are edited.
This is probably something that had first been fixed in https://github.com/edmazur/everquest-robot-stanvern/commit/9c4a970fc93bc2f3943a512ac3c7ef2b86502103, but then broken again in https://github.com/edmazur/everquest-robot-stanvern/commit/20ccd277f523939c51001358d2642a8a83d6b34a when the Javacord API changed.

The hedging ("try", "probably") in this commit message is because I've been unable to setup an environment in which I can reproduce this bug reliably. Disabling the Javacord cache doesn't seem to work (if that's even possible).

src/main/java/com/edmazur/eqrs/discord/Discord.java
@@ -201,6 +201,10 @@ public class Discord {
     return false;
   }
 
+  public CompletableFuture<Message> getMessage(long id, TextChannel textChannel) {
+    return discordApi.getMessageById(id, textChannel);
+  }
+
   private User getUser(DiscordUser discordUser) {
     return discordApi.getUserById(discordUser.getId()).join();
   }

src/main/java/com/edmazur/eqrs/discord/listener/AuditListener.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.mention.AllowedMentionsBuilder;
@@ -82,11 +83,15 @@ public class AuditListener implements MessageCreateListener, MessageEditListener
 
   @Override
   public void onMessageEdit(MessageEditEvent event) {
+    // This step feels a little hacky, but for some reason the last edit timestamp isn't populated
+    // on the MessageEditEvent, so forcible re-request the full message so you have it.
+    Message message = discord.getMessage(event.getMessageId(), event.getChannel()).join();
+
     onMessage(
-        event.getChannel(),
-        event.getMessage().getLastEditTimestamp().get(),
-        event.getMessageAuthor(),
-        event.getMessageContent(),
+        message.getChannel(),
+        message.getLastEditTimestamp().get(),
+        message.getAuthor(),
+        message.getContent(),
         MessageType.EDIT);
   }
 