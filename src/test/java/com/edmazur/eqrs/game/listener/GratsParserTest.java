94c9977645a6b4491032457d34731fc7ad06b446
Ed Mazur <edmazur@gmail.com>
2023-08-10 22:01:22 -0400

    Give better error messages for failed assertions in GratsParserTest.
    Currently if the parse outcome differs (i.e. failed vs. succeeded), the diff is just against "null" which isn't very helpful.

--- src/test/java/com/edmazur/eqrs/game/listener/GratsParserTest.java
+++ src/test/java/com/edmazur/eqrs/game/listener/GratsParserTest.java
@@ -1,6 +1,7 @@
 package com.edmazur.eqrs.game.listener;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mockConstruction;
 
 import com.edmazur.eqlp.EqLogEvent;
@@ -62,7 +63,7 @@ class GratsParserTest {
         "[Wed May 24 23:00:41 2023] Veriasse tells the guild, "
             + "'!grats Resplendent Robe Veriasse 69'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Resplendent Robe Veriasse 69", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Resplendent Robe Veriasse 69");
   }
 
   @Test
@@ -71,7 +72,7 @@ class GratsParserTest {
         "[Mon Jun 05 16:35:09 2023] Nightwyn tells the guild, "
         + "'!gratss Daewin Resplendent Wristguard 20'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Resplendent Wristguard Daewin 20", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Resplendent Wristguard Daewin 20");
   }
 
   @Test
@@ -80,7 +81,7 @@ class GratsParserTest {
         "[Thu May 25 22:37:35 2023] Darkace tells the guild, "
         + "'!grats Darkace Belt of Contention 0dkp'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Belt of Contention Darkace 0", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Belt of Contention Darkace 0");
   }
 
   @Test
@@ -89,7 +90,7 @@ class GratsParserTest {
         "[Thu May 25 22:37:35 2023] Darkace tells the guild, "
         + "'!grats Darkace Belt of Contention 0 dkp'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Belt of Contention Darkace 0", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Belt of Contention Darkace 0");
   }
 
   @Test
@@ -98,7 +99,7 @@ class GratsParserTest {
         "[Wed May 24 22:56:36 2023] Bigdumper tells the guild, "
         + "'!Grats  Nature's Melody 650 Closed Bigdumper'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Nature's Melody Bigdumper 650", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Nature's Melody Bigdumper 650");
   }
 
   @Test
@@ -107,7 +108,7 @@ class GratsParserTest {
         "[Thu Jun 08 01:52:21 2023] Screeching tells the guild, "
         + "'!grats Klandicar's Talisman Raging 1000 swapping'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Klandicar's Talisman Raging 1000", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Klandicar's Talisman Raging 1000");
   }
 
   @Test
@@ -116,10 +117,9 @@ class GratsParserTest {
         "[Wed May 24 22:56:36 2023] Bigdumper tells the guild, "
         + "'!Grats  Nature's Melody 650 blah Bigdumper'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
+    assertFailedParse(
         "``$loot Nature's Melody ??? 650`` "
-            + "(Multiple name candidates found: ``blah``, ``Bigdumper``)",
-        lootCommandOrError.getError());
+            + "(Multiple name candidates found: ``blah``, ``Bigdumper``)");
   }
 
   @Test
@@ -128,10 +128,9 @@ class GratsParserTest {
         "[Wed May 24 22:56:14 2023] Britters tells the guild, "
         + "'!Grats Braid of Golden Hair Bobbydobby 333 (britters alt)'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
+    assertFailedParse(
         "``$loot Braid of Golden Hair ??? 333`` "
-            + "(Multiple name candidates found: ``Bobbydobby``, ``(britters``, ``alt)``)",
-        lootCommandOrError.getError());
+            + "(Multiple name candidates found: ``Bobbydobby``, ``(britters``, ``alt)``)");
   }
 
   @Test
@@ -140,7 +139,7 @@ class GratsParserTest {
         "[Wed May 24 19:43:04 2023] Errur tells the guild, "
         + "'Wristband of the Bonecaster Sauromite 1 !grats'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Wristband of the Bonecaster Sauromite 1", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Wristband of the Bonecaster Sauromite 1");
   }
 
   @Test
@@ -149,9 +148,7 @@ class GratsParserTest {
         "[Mon May 29 00:08:38 2023] Shlimm tells the guild, "
         + "'Orb of the Infinite Void 105 !grats'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
-        "``$loot Orb of the Infinite Void ??? 105`` (No name found)",
-        lootCommandOrError.getError());
+    assertFailedParse("``$loot Orb of the Infinite Void ??? 105`` (No name found)");
   }
 
   @Test
@@ -160,10 +157,9 @@ class GratsParserTest {
         "[Tue May 23 17:31:38 2023] Faldimir tells the guild, "
         + "'!grats Spear of Fate 400 Hamfork / Guzmak'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
+    assertFailedParse(
         "``$loot Spear of Fate ??? 400`` "
-            + "(Multiple name candidates found: ``Hamfork``, ``Guzmak``)",
-        lootCommandOrError.getError());
+            + "(Multiple name candidates found: ``Hamfork``, ``Guzmak``)");
   }
 
   @Test
@@ -172,7 +168,7 @@ class GratsParserTest {
         "[Tue May 23 17:31:38 2023] Faldimir tells the guild, "
         + "'!grats Spear of Fate 400 HAMFORK'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Spear of Fate Hamfork 400", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Spear of Fate Hamfork 400");
   }
 
   @Test
@@ -181,9 +177,7 @@ class GratsParserTest {
         "[Sun Jun 04 23:32:34 2023] Mccreary tells the guild, "
         + "'!grats Abashi's Rod of Disempowerment mccreary 3333'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
-        "$loot Abashi's Rod of Disempowerment Mccreary 3333",
-        lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Abashi's Rod of Disempowerment Mccreary 3333");
   }
 
   @Test
@@ -192,7 +186,7 @@ class GratsParserTest {
         "[Thu Jun 15 21:55:21 2023] Trys tells the guild, "
         + "'!grats Spiroc Wingblade 1 Trys .'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Spiroc Wingblade Trys 1", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Spiroc Wingblade Trys 1");
   }
 
   @Test
@@ -201,9 +195,7 @@ class GratsParserTest {
         "[Mon Aug 07 00:26:07 2023] Gnough tells the guild, "
         + "'!grats Vyemm's Right Eye 175  (sizar)'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals(
-        "Name candidate contains non-alpha characters: ``(sizar)``",
-        lootCommandOrError.getError());
+    assertFailedParse("Name candidate contains non-alpha characters: ``(sizar)``");
   }
 
   @Test
@@ -212,7 +204,42 @@ class GratsParserTest {
         "[Tue Aug 08 22:46:11 2023] Ezzani tells the guild, "
         + "'!grats Amethyst Amulet Ezzani 1.'").get();
     gratsParser.parse(eqLogEvent);
-    assertEquals("$loot Amethyst Amulet Ezzani 1", lootCommandOrError.getValue());
+    assertSuccessfulParse("$loot Amethyst Amulet Ezzani 1");
+  }
+
+  private enum ParseOutcome {
+    SUCCESS,
+    FAILURE,
+  }
+
+  private void assertSuccessfulParse(String expectedOutput) {
+    assertParse(expectedOutput, ParseOutcome.SUCCESS);
+  }
+
+  private void assertFailedParse(String expectedOutput) {
+    assertParse(expectedOutput, ParseOutcome.FAILURE);
+  }
+
+  private void assertParse(String expectedOutput, ParseOutcome expectedParseOutcome) {
+    switch (expectedParseOutcome) {
+      case SUCCESS:
+        if (lootCommandOrError.getValue() == null) {
+          fail(String.format("Expected successful parse (%s), but it failed (%s)",
+              expectedOutput, lootCommandOrError.getError()));
+        } else {
+          assertEquals(expectedOutput, lootCommandOrError.getValue());
+        }
+        break;
+      case FAILURE:
+        if (lootCommandOrError.getError() == null) {
+          fail(String.format("Expected failed parse (%s), but it succeeded (%s)",
+              expectedOutput, lootCommandOrError.getValue()));
+        } else {
+          assertEquals(expectedOutput, lootCommandOrError.getError());
+        }
+        break;
+      default:
+    }
   }
 
 }
