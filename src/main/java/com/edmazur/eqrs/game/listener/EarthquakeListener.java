9c818bb2f66c8eb676494073d8452e6702f87256
Ed Mazur <edmazur@gmail.com>
2023-09-30 17:23:33 -0400

    Route earthquake announcements to batphone channel (instead of raid discussion).
    This also adds a 1 hour rate limit as a safeguard, as well as tweaks the message slightly.

--- src/main/java/com/edmazur/eqrs/game/listener/EarthquakeListener.java
+++ src/main/java/com/edmazur/eqrs/game/listener/EarthquakeListener.java
@@ -3,17 +3,27 @@ package com.edmazur.eqrs.game.listener;
 import com.edmazur.eqlp.EqLogEvent;
 import com.edmazur.eqlp.EqLogListener;
 import com.edmazur.eqrs.Config;
+import com.edmazur.eqrs.RateLimiter;
 import com.edmazur.eqrs.discord.Discord;
 import com.edmazur.eqrs.discord.DiscordChannel;
+import java.time.Duration;
 
 public class EarthquakeListener implements EqLogListener {
 
-  private static final DiscordChannel PROD_CHANNEL = DiscordChannel.GG_RAID_DISCUSSION;
-  private static final DiscordChannel TEST_CHANNEL = DiscordChannel.TEST_GENERAL;
+  private static final DiscordChannel PROD_CHANNEL = DiscordChannel.GG_BATPHONE;
+  private static final DiscordChannel TEST_CHANNEL = DiscordChannel.TEST_BATPHONE;
+
+  // This rate limit is just to safeguard against repeated false positive detection (code bug, GM
+  // trolling, etc.), which would result in repeated batphones. 1 hour is somewhat arbitrary - long
+  // enough that it shouldn't result in spam, but short enough where some edge case scenario where
+  // it quakes twice in a day ishandled correctly.
+  private static final Duration RATE_LIMIT = Duration.ofHours(1);
 
   private final Config config;
   private final Discord discord;
   private final EarthquakeDetector earthquakeDetector;
+
+  private final RateLimiter rateLimiter = new RateLimiter(RATE_LIMIT);
 
   public EarthquakeListener(Config config, Discord discord, EarthquakeDetector earthquakeDetector) {
     this.config = config;
@@ -24,8 +34,10 @@ public class EarthquakeListener implements EqLogListener {
   @Override
   public void onEvent(EqLogEvent eqLogEvent) {
     if (earthquakeDetector.containsEarthquake(eqLogEvent)) {
-      discord.sendMessage(
-          getChannel(), "@everyone️ Possible earthquake, ET: `" + eqLogEvent.getFullLine() + "`");
+      if (rateLimiter.getPermission()) {
+        discord.sendMessage(
+            getChannel(), "@everyone QUAKE" + "\n" + "(ET: `" + eqLogEvent.getFullLine() + "`");
+      }
     }
   }
 
